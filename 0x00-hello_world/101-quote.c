26dcc57ffe09dcfc7427d979d641f0364dd8cf96
Abanob Raffet <abanobraffet@yahoo.com>
2023-02-16 04:59:12 -0800

    file updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	char a[59]
+	= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(a, 59, 1, stderr);
 	return (1);
 }
