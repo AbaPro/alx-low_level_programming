0561ece3c3b368eb9a8a34411490cf94ee6c941a
Abanob Raffet <abanobraffet@yahoo.com>
2023-02-17 04:30:55 -0800

    file updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@
 
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = '0'; i < '9'; i++)
 	{
-		for (j = i+1; j <= '9'; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if (i + j != '9'+'8')
+			if (i + j != '9' + '8')
 			{
 				putchar(',');
 				putchar(' ');
