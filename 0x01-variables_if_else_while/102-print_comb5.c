ca6d68b93ef9b5c84edd4e71c452bb6b6747881a
Abanob Raffet <102380881+AbaPro@users.noreply.github.com>
2023-02-18 13:45:35 +0200

    Add files via upload

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int i, j, h, y;
+
+    for (i = '0'; i <= '9'; i++)
+    {
+        for (j = '0'; j <= '9'; j++)
+        {
+            for (h = '0'; h <= '9'; h++)
+            {
+                for (y = '0'; y <= '9'; y++)
+                {
+                    putchar(i);
+                    putchar(j);
+                    putchar(' ');
+                    putchar(h);
+                    putchar(y);
+                    if (i + j + h + y != '9' + '9' + '9' + '9')
+                    {
+                        putchar(',');
+                        putchar(' ');
+                    }
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
+}
